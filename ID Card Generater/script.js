8dbb33d7aee5cf8d71ee3a50c8933e88b7d89413
MANISH SHARMA <54857674+aryamanish1234@users.noreply.github.com>
2021-02-27 23:08:08 +0530

    Add files via upload

---
+++ ID Card Generater/script.js
@@ -0,0 +1,25 @@
+function IDGenerte(){
+  document.getElementById("idcard").style.display="flex";
+  nam=document.getElementById("stname").value;
+  clg=document.getElementById("college").value;
+  addrs=document.getElementById("adress").value;
+  document.getElementById("nameshow").innerHTML=`Name: ${nam}`;
+  document.getElementById("collegeshow").innerHTML=`College: ${clg}`;
+  document.getElementById("adressshow").innerHTML=`Adress: ${addrs}`;
+}
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      $('#imgPrev').attr('src', e.target.result);
+      $('#idCardImg').attr('src', e.target.result);
+    }
+   
+    
+    reader.readAsDataURL(input.files[0]); 
+  }
+}
+$("#imgInp").change(function() {
+  readURL(this);
+});
